6c00503592528fb973cf9532892750ef3e48b149
cdyremix <c.borawa@gmail.com>
2025-12-06 16:41:15 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,6 @@
 module.exports = async (req, res) => {
-  const { token, skip, take, order, from, to } = req.query;
+  const { skip, take, order, from, to } = req.query;
+  const token = '1af8f105-cc07-4168-b092-6dcd8a47838e'; // Hardcoded here
   const apiUrl = `https://api.skinrave.gg/affiliates/public/applicants?token=${token}&skip=${skip}&take=${take}&order=${order}&from=${from}&to=${to}`;
 
   try {
